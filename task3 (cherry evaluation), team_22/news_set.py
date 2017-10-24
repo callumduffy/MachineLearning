2a81eecab1be7428065b904837de634c6633b84c
Callum Duffy <duffyc8@tcd.ie>
2017-10-24 11:32:36 +0100

    updated and renamed news set file

--- task3 (cherry evaluation), team_22/news_set.py
+++ task3 (cherry evaluation), team_22/news_set.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import model_selection
-from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
@@ -12,7 +11,8 @@ from sklearn.gaussian_process.kernels import ConstantKernel, RBF
 dataset = pd.read_csv("OnlineNewsPopularity.csv", delimiter=", ").drop(['url'], axis=1)
 
 X = dataset
-Y = dataset['']
+Y = dataset['shares']
+
 X= X.astype('int')
 Y=Y.astype('int')
 
@@ -20,22 +20,22 @@ Y=Y.astype('int')
 # prepare models, one novel and 3 baseline
 
 models = []
+#novel below
 models.append(('KNN', KNeighborsClassifier()))
 models.append(('CART', DecisionTreeClassifier()))
-models.append(('GP', GaussianProcessClassifier()))
-models.append(('SGD', SGDClassifier()))
+models.append(('GPC', GaussianProcessClassifier()))
+models.append(('SGDC', SGDClassifier()))
 
 # evaluate each model in turn
 results = []
 names = []
-scoring = 'popularity'
+scoring = 'accuracy'
 
 for name, model in models:
-
     kfold = model_selection.KFold(n_splits=10)
-	cv_results = model_selection.cross_val_score(model, X[:size], Y[:size], cv=kfold, scoring=scoring)
+    cv_results = model_selection.cross_val_score(model, X[:size], Y[:size], cv=kfold, scoring=scoring)
     results.append(cv_results)
-    names.append(name)
-    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-    print(msg)
+   	names.append(name)
+   	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+   	print(msg)
 # boxplot algorithm comparison
