0b4bb3db520698415f0d9610c256a74ac9b5264f
Kevin Morris <morrisk3@tcd.ie>
2017-10-24 12:32:58 +0100

    Adding the 5 evaluation metrics

--- task3 (cherry evaluation), team_22/fashion_set.py
+++ task3 (cherry evaluation), team_22/fashion_set.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 from sklearn import model_selection
+from sklearn import metrics
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.linear_model import SGDClassifier
-from sklearn.gaussian_process.kernels import ConstantKernel, RBF
+from sklearn.linear_model import LogisticRegression
 
 dataset = pd.read_csv("fashion-mnist_train.csv", delimiter=",")
 
@@ -18,18 +19,36 @@ Y=Y.astype('int')
 models = []
 #novel below
 models.append(('KNN', KNeighborsClassifier())) 
+#baselines
+models.append(('LR', LogisticRegression()))
 models.append(('CART', DecisionTreeClassifier()))
-models.append(('GPC', GaussianProcessClassifier()))
 models.append(('SGDC', SGDClassifier()))
 
-results = []
-names = []
-scoring = 'accuracy'
+scoring = ['accuracy', 'homogeneity_score', 'mutual_info_score', 'completeness_score', 'v_measure_score']
+
+size = 100
 
 for name, model in models:
+
     kfold = model_selection.KFold(n_splits=10)
-    cv_results = model_selection.cross_val_score(model, X[:size], Y[:size], cv=kfold, scoring=scoring)
-    results.append(cv_results)
-   	names.append(name)
-   	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-   	print(msg)
+    start = time.time()
+    cv_results = model_selection.cross_val_score(model, X[:size], Y[:size], cv=kfold, scoring=scoring[0])
+    cv_results1 = model_selection.cross_val_score(model, X[:size], Y[:size], cv=kfold, scoring=scoring[1])
+    cv_results2 = model_selection.cross_val_score(model, X[:size], Y[:size], cv=kfold, scoring=scoring[2])
+    cv_results3 = model_selection.cross_val_score(model, X[:size], Y[:size], cv=kfold, scoring=scoring[3])
+    cv_results4 = model_selection.cross_val_score(model, X[:size], Y[:size], cv=kfold, scoring=scoring[4])
+    end = time.time()
+
+    msg = "%s: \n%s: \n%s: %f (%f) " % (name, "5 Common Evaluation Metrics ", "Accuracy" ,  cv_results.mean(), cv_results.std())
+    msg1 = "%s: %f (%f) " % ("homogeneity_score" ,  cv_results1.mean(), cv_results1.std())
+    msg2 = "%s: %f (%f) " % ("mutual_info_score" ,  cv_results2.mean(), cv_results2.std())
+    msg3 = "%s: %f (%f) " % ("completeness_score" ,  cv_results3.mean(), cv_results3.std())
+    msg4 = "%s: %f (%f) " % ("v_measure_score" ,  cv_results4.mean(), cv_results4.std())
+    msg5 = "%s: \n%s: %f" % (" 2 New Evaluation Metrics", "Times (seconds)" , (end-start) )
+    #msg5 = "%s: %f" % ("Times (seconds)" , (end-start) )
+    print(msg, "\n",msg1 ,"\n",msg2,"\n",msg3,"\n",msg4,"\n",msg5, "\n" )
+
+
+
+    # [, 'adjusted_rand_score', 'average_precision', 'completeness_score', 'explained_variance', 'f1', 'f1_macro', 'f1_micro', 'f1_samples', 'f1_weighted', 'fowlkes_mallows_score', 'homogeneity_score', 'mutual_info_score', 'neg_log_loss', 'neg_mean_absolute_error', 'neg_mean_squared_error', 'neg_mean_squared_log_error', 'neg_median_absolute_error', 'normalized_mutual_info_score', 'precision', 'precision_macro', 'precision_micro', 'precision_samples', 'precision_weighted', 'r2', 'recall', 'recall_macro', 'recall_micro', 'recall_samples', 'recall_weighted', 'roc_auc', 'v_measure_score']
+    
