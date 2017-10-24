1592b2155d577dd6f0ea3139fe83ae79752539db
Joseph Fitzpatrick <jfitzpa1@tcd.ie>
2017-10-24 13:01:02 +0100

    updated

--- task1 (performance by dataset size), team_22/fashionDataSet.py
+++ task1 (performance by dataset size), team_22/fashionDataSet.py
@@ -1,30 +1,24 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import model_selection
 from sklearn.linear_model import LogisticRegression, LinearRegression, Ridge
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
-
 # load dataset
 dataset = pd.read_csv("fashion-mnist_train.csv", delimiter=",")
-
 sizes = [100, 500, 1000, 5000,10000]
-
 X = dataset
 Y = dataset['label']
-
 X= X.astype('int')
 Y=Y.astype('int')
-# prepare configuration for cross validation test harness
-# prepare models
 models = []
 models.append(('LogisticRegressionR', LogisticRegression(), 0))
 models.append(('KNN', KNeighborsClassifier(),0))
 models.append(('Linear Regression', LinearRegression(),1))
 models.append(('Ridge Regression', Ridge(alpha = .5),1))
 # evaluate each model in turn
-
 scoring = ['accuracy','neg_mean_squared_error']
 
 for size in sizes:
