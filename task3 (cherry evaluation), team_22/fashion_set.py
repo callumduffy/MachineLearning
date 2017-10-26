3d69c8009627a4d0226f199a36f039afc4822fa7
Kevin Morris <morrisk3@tcd.ie>
2017-10-26 20:29:26 +0100

    Adding the new evaluation metric to the fashion set

--- task3 (cherry evaluation), team_22/fashion_set.py
+++ task3 (cherry evaluation), team_22/fashion_set.py
@@ -25,7 +25,7 @@ models.append(('LR', LogisticRegression()))
 models.append(('DTC', DecisionTreeClassifier()))
 models.append(('SGDC', SGDClassifier()))
 
-scoring = ['accuracy', 'homogeneity_score', 'precision', 'completeness_score', 'v_measure_score']
+scoring = ['accuracy', 'homogeneity_score', 'normalized_mutual_info_score', 'completeness_score', 'v_measure_score']
 
 size = 1000
 
@@ -34,59 +34,17 @@ for name, model in models:
 
     for score in scoring:
         kfold = model_selection.KFold(n_splits=10)
-        start = time.time()
-        cv_results = model_selection.cross_val_score(model, X[:size], Y[:size], cv=kfold, scoring=score)
-        end = time.time()
-        runtime = end-start
 
-        msg1 = "%s: %f (%f) " % (score ,  cv_results.mean(), cv_results.std())
-        print("Time (score/cpu-time): %f" %(cv_results.mean()/runtime))
-        print(msg1)
-
-    
-import pandas as pd
-import matplotlib.pyplot as plt
-import time
-import psutil
-from sklearn import model_selection
-from sklearn import metrics
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import SGDClassifier
-from sklearn.linear_model import LogisticRegression
-
-dataset = pd.read_csv("fashion-mnist_train.csv", delimiter=",")
-
-X = dataset
-Y = dataset['label']
-
-X= X.astype('int')
-Y=Y.astype('int')
-
-models = []
-#novel below
-models.append(('KNN', KNeighborsClassifier())) 
-#baselines
-models.append(('LR', LogisticRegression()))
-models.append(('DTC', DecisionTreeClassifier()))
-models.append(('SGDC', SGDClassifier()))
-
-scoring = ['accuracy', 'homogeneity_score', 'mutual_info_score', 'completeness_score', 'v_measure_score']
-
-size = 1000
-
-for name, model in models:
-    print("\n%s" %(name))
-
-    for score in scoring:
-        kfold = model_selection.KFold(n_splits=10)
-        start = time.time()
-        cv_results = model_selection.cross_val_score(model, X[:size], Y[:size], cv=kfold, scoring=score)
-        end = time.time()
-        runtime = end-start
-
-        msg1 = "%s: %f (%f) " % (score ,  cv_results.mean(), cv_results.std())
-        print("Time (score/cpu-time): %f" %(cv_results.mean()/runtime))
+        if score == 'accuracy':
+            start = time.time()
+            cv_results = model_selection.cross_val_score(model, X[:size], Y[:size], cv=kfold, scoring=score)
+            end = time.time()
+            runtime = end-start
+            timescore = cv_results.mean()/runtime
+            msg1 = "%s: %f (%f) Timescore: %f" % (score ,  cv_results.mean(), cv_results.std(), timescore)
+        else:
+            cv_results = model_selection.cross_val_score(model, X[:size], Y[:size], cv=kfold, scoring=score)
+            msg1 = "%s: %f (%f) " % (score ,  cv_results.mean(), cv_results.std())
         print(msg1)
 
     
